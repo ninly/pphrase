63e92906e955ca4aa18f9fa915be265306c13f1c
Jason Conklin <j@ninly.net>
2014-02-05 07:36:45 -0600

    bring in word list as submodule

--- pphrase.py
+++ pphrase.py
@@ -3,14 +3,13 @@ pphrase.py : generate 4-word random passphrase from 10,000 most common words in
 Google N-grams, discounting words <= 2 letters long. 
 
 TODO:
-  * generate directly from google-10000-english file
   * allow for command-line argument to indicate phassphrase length in words
 
 """
 
 import random
 
-def getwords(filename = 'google-10000-english.txt'):
+def getwords(filename = 'wordlist/google-10000-english.txt'):
     with open(filename,'r') as f:
         lines = list(f)
     f.close()
