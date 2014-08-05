6ffdc00437b6630be95a5c95c3c6051d40e679a3
Jason Conklin <j@ninly.net>
2014-08-05 16:03:40 -0500

    version 0.0.3

--- pphrase.py
+++ pphrase.py
@@ -1,9 +1,8 @@
-"""
-pphrase.py :
+""" pphrase.py : Generate a random passphrase
 
-    Generate a random passphrase from the most common words (max 10000)
-    in Google's trillion-word corpus. See http://xkcd.com/936 for the
-    motivation and inspiration.
+    Generate a random passphrase from a subset of the most common
+    words (max 10000) in Google's trillion-word corpus. See
+    http://xkcd.com/936 for the motivation and inspiration.
 
     Licensed under terms of MIT license (see LICENSE-MIT)
     Copyright (c) 2014 Jason Conklin, <j@ninly.net>
@@ -22,7 +21,6 @@ Options:
                                     [default: 2].
     -p POOL --poolsize=POOL         Select from most common POOL words
                                     [default: 2048].
-
 """
 
 import os
@@ -32,32 +30,84 @@ from docopt import docopt
 basedir = os.path.dirname(os.path.abspath(__file__))+'/'
 
 if __name__ == "__main__":
-    arguments = docopt(__doc__, version='0.0.2')
+    arguments = docopt(__doc__, version='0.0.3')
 
-def getwords(filename = basedir+'wordlist/google-10000-english.txt'):
+
+class ArgError(Exception):
+    """Error class with no ability to can."""
+    def __init__(self, value="could not even."):
+        self.value = value
+    def __str__(self):
+        return str(self.value)
+
+
+def sanitize_args():
+    """Check input args for sanity."""
+    try:
+        numwords = int(arguments['--words'])
+        poolsize = int(arguments['--poolsize'])
+        minword = int(arguments['--minword'])
+        maxword = int(arguments['--maxword'])
+    except ValueError:
+        print("Error: Option arguments must be integers.")
+        return 1
+
+    try:
+        if (minword < 1) or (maxword < 1) or (numwords < 1):
+            raise ArgError("word count and length must be positive integers.")
+        if (poolsize > 10000) or (poolsize < 1):
+            raise ArgError("pool size must be between 1 and 10000.")
+    except ArgError as e:
+        print('Could not even: {}'.format(e))
+        return 1
+
+    return 0
+
+
+def get_pool(filename = basedir+'wordlist/google-10000-english.txt'):
+    """Generate word pool to user specifications."""
     poolsize = int(arguments['--poolsize'])
     minword = int(arguments['--minword'])
     maxword = int(arguments['--maxword'])
-
     with open(filename,'r') as f:
-        lines = list(f)[:poolsize]
+        lines = list(f)
     f.close()
 
     words = list()
 
+    # cull outsized words
     for line in lines:
         if len(line.strip()) >= minword and len(line.strip()) <= maxword:
             words.append(line.strip())
+    # only keep poolsize words
+    try:
+        if len(words) < poolsize:
+            # words_avail = len(words)
+            raise ArgError("only "+str(len(words))+" words in specified length range.")
+    except ArgError as e:
+        print('Could not even: {}'.format(e))
+        return
+    except:
+        return
+    else:
+        words = list(words)[:poolsize]
 
     return words
 
+
 def main():
+    """Output passphrase."""
+    try:
+        if sanitize_args(): raise ArgError
+    except ArgError:
+        return
+
     numwords = int(arguments['--words'])
-    # print(arguments)
-    # numwords = 4
-    words = getwords()
-    for i in range(numwords):
-        print random.choice(words),
+    wordpool = get_pool()
+    if wordpool:
+        for i in range(numwords):
+            print random.choice(wordpool),
+
 
 if __name__ == "__main__":
     main()
